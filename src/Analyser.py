ffb90867dd7db48b7a4ff4b20778f6bbab644907
Leonardo Broch de Morais <leo_broch@hotmail.com>
2016-11-12 19:02:55 -0200

    Adicionada funcao contaPalavrasTexto e parcialmente concluido exercicio

--- src/Analyser.py
+++ src/Analyser.py
@@ -1,5 +1,14 @@
 def leArquivo(arq):
 	linhas = arq.readlines()
-	print (linhas)
+	contaPalavrasTexto(linhas)
+
+def contaPalavrasTexto(linhas):
+	palavrasTexto = {}
+	for linha in linhas:
+		palavras = linha.split(' ') 
+		for palavra in palavras:
+			palavrasTexto[palavra] = palavrasTexto.get(palavra, 0) + 1
+
+	print (palavrasTexto)
 	
 	
